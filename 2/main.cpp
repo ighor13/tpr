56bcd6bd1301c6d42f1f4cfb779d32484abe5abb
ighor13 <ighor@ighor.ru>
2025-11-25 15:32:49 +0300

    Cosmetic Fix

--- 2/main.cpp
+++ 2/main.cpp
@@ -8,10 +8,10 @@ typedef unsigned long int natural;
 
 real fact(const natural n) // да, факториал - натуральное число, но происходит потеря точности при больших n для unsigned long int, поэтому long double
 {
-    if(n==0)
-	return 1;
-    else
-	return n*fact(n-1);
+	if(n==0)
+		return 1;
+	else
+		return n*fact(n-1);
 }
 
 real sum(const real alpha,const natural n);
@@ -23,17 +23,16 @@ real p(const real alpha,const natural n)
 
 real sum(const real alpha,const natural n)
 {
-    if(n==0)
-	return pow(alpha,n)/fact(n); // const=1
-    else
-	return pow(alpha,n)/fact(n)+sum(alpha,n-1);
+	if(n==0)
+		return pow(alpha,n)/fact(n); // const=1
+	else
+		return pow(alpha,n)/fact(n)+sum(alpha,n-1);
 }
 
 int main(int argc, char* argv[])
 {
-    real mu=0.3;
-
-    real a[][3] = // <- 3 rows
+	real mu=0.3;
+	real a[][3] = // <- 3 rows
 		{ 
 		    {0.4,0.3,0.3},
 		    {0.3,0.4,0.3},
@@ -49,16 +48,16 @@ int main(int argc, char* argv[])
 
     //a [row] [col]
 
-    std::cout.precision(8); // <- выводим N знаков после запятой
+	std::cout.precision(8); // <- выводим N знаков после запятой
 
-    real pn=0;
-    for(natural i=0;i<sizeof a/sizeof a[0];i++,cout<<endl) // rows
-	for(natural j=0;j<sizeof a[0]/sizeof(a[0][0]);j++,pn=0) // cols
-	{
-	    for(natural k=0;k<sizeof a[0] / sizeof(a[0][0]);k++) // cols
-		pn+=a[i][k]*p(a[i][k]/mu,b[j][k]);
-	    cout<<pn<<"\t";
-	}
+	real pn=0;
+	for(natural i=0;i<sizeof a/sizeof a[0];i++,cout<<endl) // rows
+		for(natural j=0;j<sizeof a[0]/sizeof(a[0][0]);j++,pn=0) // cols
+		{
+			for(natural k=0;k<sizeof a[0] / sizeof(a[0][0]);k++) // cols
+				pn+=a[i][k]*p(a[i][k]/mu,b[j][k]);
+			cout<<pn<<"\t";
+		}
 
-    return 0;
+	return 0;
 }
